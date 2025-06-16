6fc027ac8e47a4cae84987d3b569617d62a92c01
LeonardoDiFranco29 <leonardodanieldifranco@gmail.com>
2025-06-15 21:07:36 -0300

    test tipo insecto mock

--- test/CriterioTipoInsectoTest.java
+++ test/CriterioTipoInsectoTest.java
@@ -1,12 +1,52 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-class CriterioTipoInsectoTest {
+public class CriterioTipoInsectoTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Mock
+    private Muestra muestraMock;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCumpleCuandoResultadoActualEsElTipoInsectoRequerido() {
+        CriterioTipoInsecto criterio = new CriterioTipoInsecto(TipoOpinion.VINCHUCA);
+
+        when(muestraMock.resultadoActualOpiniones()).thenReturn(TipoOpinion.VINCHUCA);
+
+        assertTrue(criterio.cumple(muestraMock));
+
+        verify(muestraMock).resultadoActualOpiniones();
+    }
+
+    @Test
+    void testNoCumpleCuandoResultadoActualNoEsElTipoInsectoRequerido() {
+        CriterioTipoInsecto criterio = new CriterioTipoInsecto(TipoOpinion.VINCHUCA);
+
+        when(muestraMock.resultadoActualOpiniones()).thenReturn(TipoOpinion.CHINCHE_FOLIADA);
+
+        assertFalse(criterio.cumple(muestraMock));
+
+        verify(muestraMock).resultadoActualOpiniones();
+    }
+
+    @Test
+    void testNoCumpleCuandoResultadoEsNingunaPeroSeEsperaOtroTipo() {
+        CriterioTipoInsecto criterio = new CriterioTipoInsecto(TipoOpinion.CHINCHE_FOLIADA);
+
+        when(muestraMock.resultadoActualOpiniones()).thenReturn(TipoOpinion.NINGUNA);
+
+        assertFalse(criterio.cumple(muestraMock));
+
+        verify(muestraMock).resultadoActualOpiniones();
+    }
 }
